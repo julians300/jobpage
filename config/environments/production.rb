b610bd94f6afc74936889480d79326f7b4543793
Julian Salas <julians300@gmail.com>
2013-09-10 02:26:42 -0500

    update devise for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,6 @@ Jobpage::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # devise
+  config.assets.initialize_on_precompile = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 end
